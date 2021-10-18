497bc07a84be8ad860905277bb62b20fc8bd392a
DennisTarik <dm499555@gmail.com>
2021-10-18 09:31:04 +0200

    Move local git repository

---
+++ main.js
@@ -0,0 +1,30 @@
+import { createElement } from "./lib/elements.js";
+import { createTitleComponent } from "/lib/titleComponent.js";
+import { createFooterComponent } from "/lib/footerComponent.js";
+import { createTodoComponent } from "/lib/todoComponent.js";
+
+function createApp() {
+    const appElement = document.querySelector("#app");
+  
+    const titleComponent = createTitleComponent();
+    
+    const footerComponent = createFooterComponent();
+
+    const formElement = createTodoComponent (function (task) {
+
+      let answerText = createElement(
+        "p", {
+          className: "answer"
+        },
+        [`${task}`]
+      ); 
+    appElement.append(answerText);
+    }
+  );
+  
+    appElement.append(titleComponent);
+    appElement.append(formElement);
+    appElement.append(footerComponent);
+}
+
+createApp ();
